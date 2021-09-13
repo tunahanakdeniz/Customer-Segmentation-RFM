60269db31a21020e6d500935b8f1dd1aeb8bec88
Tunahan Akdeniz <64337935+tunahanakdeniz@users.noreply.github.com>
2021-09-13 03:10:50 +0300

    Update RFM-CustomerSegmentation.py

--- RFM-CustomerSegmentation.py
+++ RFM-CustomerSegmentation.py
@@ -7,66 +7,45 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
-"""###Görev 1.1
-###Online Retail II excelindeki 2010-2011 verisini okuyunuz. Oluşturduğunuz dataframe’in kopyasını oluşturunuz.
-"""
-
 df_ = pd.read_excel("online_retail_II.xlsx", sheet_name="Year 2010-2011")
 df = df_.copy()
-
 df.head()
 
-"""###Görev 1.2
-### Verinin betimsel istatistiklerini inceleyiniz.
-"""
+
+#Verinin betimsel istatistikleri
 
 df.describe().T
 
-"""###Görev 1.3
-###Veri setinde eksik gözlem var mı? Varsa hangi değişkende kaç tane eksik gözlem vardır?
-"""
+#Veri setinde eksik gözlem var mı? Varsa hangi değişkende kaç tane eksik gözlem vardır
 
 df.isnull().sum()
 
-"""###Görev 1.4
-###Eksik gözlemleri veri setinden çıkartınız. Çıkarma işleminde ‘inplace=True’ parametresini kullanınız.
-"""
+#Eksik gözlemleri veri setinden çıkartma
 
 df.dropna(inplace=True)
 
-"""###Görev 1.5
-### Eşsiz ürün sayısı kaçtır?
-"""
+#Eşsiz ürün sayısı kaçtır?
 
 df["Description"].nunique()
 
-"""###Görev 1.6
-###Hangi üründen kaçar tane vardır?
-"""
+#Hangi üründen kaçar tane vardır?
 
 df["Description"].value_counts().head()
 
-"""###Görev 1.7
-###En çok sipariş edilen 5 ürünü çoktan aza doğru sıralayınız.
-"""
+#En çok sipariş edilen 5 ürün
 
 df.groupby("Description").agg({"Quantity": "sum"}).sort_values("Quantity", ascending=False).head()
 
-"""###Görev 1.8
-###Faturalardaki ‘C’ iptal edilen işlemleri göstermektedir. İptal edilen işlemleri veri setinden çıkartınız.
-"""
+#Faturalardaki ‘C’ iptal edilen işlemleri göstermektedir. İptal edilen işlemleri veri setinden çıkartma
 
 df = df[~df["Invoice"].str.contains("C", na=False)]
 
-"""###Görev 1.8
-###Fatura başına elde edilen toplam kazancı ifade eden ‘TotalPrice’ adında bir değişken oluşturunuz.
-"""
+#Fatura başına elde edilen toplam kazancı ifade eden ‘TotalPrice’ adında bir değişken oluşturma
 
 df["TotalPrice"] = df["Quantity"] * df["Price"]
-
 df.head()
 
-"""##Görev 2-RFM Metriklerinin Hazırlanması"""
+#RFM Metriklerinin Hazırlanması
 
 df["InvoiceDate"].max()
 today_date = dt.datetime(2011, 12, 11)
@@ -80,7 +59,7 @@ rfm = rfm[(rfm['monetary'] > 0)]
 
 rfm.head()
 
-"""##Görev 3-RFM Skorlarının Oluşturulması ve Tek Bir Değişkene Çevrilmesi"""
+#RFM Skorlarının Oluşturulması ve Tek Bir Değişkene Çevrilmesi
 
 rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
 rfm["frequency_score"] = pd.qcut(rfm['frequency'].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
@@ -91,7 +70,7 @@ rfm["RFM_SCORE"] = (rfm['recency_score'].astype(str) +
 
 rfm.head()
 
-"""## Görev 4- RFM Skorlarının Segment Olarak Tanımlanması"""
+#RFM Skorlarının Segment Olarak Tanımlanması
 
 seg_map = {
     r'[1-2][1-2]': 'Hibernating',
@@ -110,12 +89,6 @@ rfm['segment'] = rfm['RFM_SCORE'].replace(seg_map, regex=True)
 
 rfm.head()
 
-"""##Görev 5.1
-### Önemli bulduğunuz 3 segmenti seçiniz. Bu üç segmenti;
-### - Hem aksiyon kararları açısından,
-### - Hem de segmentlerin yapısı açısından (ortalama RFM değerleri) yorumlayınız.
-"""
-
 rfm[["segment", "recency", "frequency", "monetary"]].groupby("segment").agg(["mean", "count"])
 
 t = rfm["segment"].value_counts()
@@ -125,18 +98,21 @@ t
 z = rfm["segment"].value_counts()
 z
 
-"""###### İlgili veri setiyle alakalı segmentasyon yapıldığında sayıca en fazla olan segmentin "Hibernating" olduğu gözlemlenmiştir. Recency ve Frequency skorlarında en düşük skora sahip bu grup için öncelikli aksiyon alınması düşünülse de uzun vadede daha fazla verim alınabilecek segmentler göze çarpmaktadır.
-
-##### Total kümenin %24.7'lik bir kısmını oluşturan "At Risk" ve "Potential Loyalist" segmentleri bu konuda ilk etapta aksiyon alınması gereken segmentler olabilir. Potential Loyalist segmentinin sık sık alışveriş yaptığı ancak bu alışveriş frekanslarının görece yüksek olmadığını görüyoruz. Bu kategoride müşteri için alınacak ilk aksiyon "Cross-sell" ya da "Upper-sell" methodları olacaktır. Müşteriye hitap eden ürünle beraber tamamlayıcı bir ürün ya da daha fazla verim alacağı bir üst ürün satışı gerçekleştirmek için pazarlama çalışmaları yapılmalıdır. Bu çalışmalar için ise ilgili ürün satışından sonra e-mail, sms ya da bildirim yoluyla cross/upper sell kategorisinde yer alan ürünler göz önünde tutulmalıdır.
-
-###### Bu %24.7'lik dilimin içerisinde yer alan bir diğer segment "At_Risk" segmentine bakıldığında hem sık sık alışveriş yapmadığı ancak yapılan alışverin yüksek frekanslı olduğunu gözlemliyoruz. Bu tarz bir segment için öncelikli olarak müşteriyi daha kısa periyotlarla alışverişe teşvik edebilecek aksiyonlar alınması önerilebilir. Bu aksiyonlara Potential Loyalist'lere uygulanan cross/upper sell methotları dahil olabileceği gibi, müşteriyle birebir iletişim ve indirim gibi daha çekici pazarlama teknikleri düşünülebilir.
-
-###### At Risk grubunun tabiri caizse bir üst modeli olan Cant Loose grubunun da getiri bakımından sıralamada 3.sırada yer aldığını gözlemliyoruz. Uzun aralıklarla ancak frekansı yüksek ve getirisi fazla alışverişleriyle dikkat çeken bu grup için de alışveriş aralıklarını kısaltacak aksiyonlar devreye sokulmalıdır. Birebir iletişim yoluyla, ya da pazarlama yapısı uygunsa üyelik avantajlarıyla bu müşteri tipinin dikkati bir şekilde alışveriş kanallarına çekilmelidir.
-
-###Görev 5.2
-###"Loyal Customers" sınıfına ait customer ID'leri seçerek excel çıktısını alınız.
+"""İlgili veri setiyle alakalı segmentasyon yapıldığında sayıca en fazla olan segmentin
+Hibernating" olduğu gözlemlenmiştir. Recency ve Frequency skorlarında en düşük skora sahip bu 
+grup için öncelikli aksiyon alınması düşünülse de uzun vadede daha fazla verim alınabilecek segmentler göze çarpmaktadır.
+Total kümenin %24.7'lik bir kısmını oluşturan "At Risk" ve "Potential Loyalist" segmentleri bu konuda ilk etapta aksiyon alınması gereken segmentler olabilir. 
+Potential Loyalist segmentinin sık sık alışveriş yaptığı ancak bu alışveriş frekanslarının görece yüksek olmadığını görüyoruz. 
+Bu kategoride müşteri için alınacak ilk aksiyon "Cross-sell" ya da "Upper-sell" methodları olacaktır. 
+Müşteriye hitap eden ürünle beraber tamamlayıcı bir ürün ya da daha fazla verim alacağı bir üst ürün satışı gerçekleştirmek için pazarlama çalışmaları yapılmalıdır.
+Bu çalışmalar için ise ilgili ürün satışından sonra e-mail, sms ya da bildirim yoluyla cross/upper sell kategorisinde yer alan ürünler göz önünde tutulmalıdır.
+Bu %24.7'lik dilimin içerisinde yer alan bir diğer segment "At_Risk" segmentine bakıldığında 
+hem sık sık alışveriş yapmadığı ancak yapılan alışverin yüksek frekanslı olduğunu gözlemliyoruz. Bu tarz bir segment için öncelikli olarak müşteriyi 
+daha kısa periyotlarla alışverişe teşvik edebilecek aksiyonlar alınması önerilebilir.
+Bu aksiyonlara Potential Loyalist'lere uygulanan cross/upper sell methotları dahil olabileceği gibi, müşteriyle birebir iletişim ve indirim gibi 
+daha çekici pazarlama teknikleri düşünülebilir.
+At Risk grubunun tabiri caizse bir üst modeli olan Cant Loose grubunun da getiri bakımından sıralamada 3.sırada yer aldığını gözlemliyoruz. 
+Uzun aralıklarla ancak frekansı yüksek ve getirisi fazla alışverişleriyle dikkat çeken bu grup için de alışveriş aralıklarını kısaltacak aksiyonlar devreye sokulmalıdır. 
+Birebir iletişim yoluyla, ya da pazarlama yapısı uygunsa üyelik avantajlarıyla bu müşteri tipinin dikkati bir şekilde alışveriş kanallarına çekilmelidir.
 """
 
-new_df = pd.DataFrame()
-new_df["Loyal_Customers_id"] = rfm[rfm["segment"] == "Loyal_Customers"].index
-new_df.to_excel("Loyal_Customers.xlsx")
